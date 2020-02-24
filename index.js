a7b8b446cd4fcc83247d033092b8ccd815d36239
NamDo <namdo10121999@gmail.com>
2020-02-24 21:25:12 +0700

    Commit all files

---
+++ index.js
@@ -0,0 +1,64 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview Page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, {
+      "Content-Type": "text/html"
+    });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+
+    // Product Page
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "Content-Type": "text/html"
+    });
+
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  }
+
+  // API
+  else if (pathname === "/api") {
+    res.writeHead(200, {
+      "Content-Type": "application/json"
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "text/html"
+    });
+    res.end("<h1>Page not found!<h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("server run on port 8000");
+});
